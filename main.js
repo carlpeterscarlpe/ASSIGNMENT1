03d104270f7706ffe8be657028cf9a0298272ab4
unknown <carlpeterscarlpe@gmail.com>
2022-06-21 12:46:34 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function getInputValue(){
+    // Selecting the input element and get its value 
+    var inputVal = document.getElementById("inputfield").value;
+    document.getElementById("display").innerHTML = "Hello   " + inputVal +" welcome to the bootcamp";
+   
+   
+}
